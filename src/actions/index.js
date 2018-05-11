0714d2c58307637ababa94870184c558bbeb6bf5
Joni Kettunen <joni.kettunen@gofore.com>
2018-05-11 10:21:35 +0300

    Latest additions to the tutorial

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+// Action creators
+export function selectBook(book) {
+  // selectBook is an ActionCreator, it needs to return an action,
+  // and object with a type property.
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+  };
+}
